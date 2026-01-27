4f461c7f62b2155bd3883f7ecb2da03e753381e8
Kodatsu <dournauxnathan@gmail.com>
2026-01-27 11:58:29 +0100

    AligmentRule.cs implémentation

---
+++ SwarmLab-UPM/Runtime/Rules/AlignmentRule.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using PlasticGui.WorkspaceWindow.PendingChanges;
+using SwarmLab;
+using UnityEditor.TerrainTools;
+using UnityEngine;
+
+namespace Runtime.Rules
+{
+    public class AlignmentRule : SteeringRule
+    {
+        public override Vector3 CalculateForce(Entity entity, List<Entity> neighbors)
+        {
+            if (neighbors == null || neighbors.Count == 0)
+                return Vector3.zero;
+            
+            Vector3 steering = Vector3.zero;
+            float totalWeight = 0f;
+            int  count = 0;
+
+            foreach (var neighbor in neighbors)
+            {
+                if (neighbor == entity) continue;
+                
+                float weight = GetWeightFor(neighbor.Species);
+                if (weight > 0f)
+                {
+                    steering += neighbor.Velocity * weight;
+                    totalWeight += weight;
+                    count++;
+                }
+
+                if (count > 0 && totalWeight > 0f)
+                {
+                    steering /= count;
+                    Vector3 direction = steering - entity.Position;
+                    return direction.normalized;
+                }
+            }
+            
+            return Vector3.zero;
+        }
+    }
+}
