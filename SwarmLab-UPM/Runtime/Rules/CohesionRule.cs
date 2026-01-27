51adbfdb9eb73a68f6b32f82874fda3a2e5281ea
Kodatsu <dournauxnathan@gmail.com>
2026-01-27 12:30:25 +0100

    Revert "Merge branch 'main' of https://github.com/xaxam2001/SwarmLab"
    This reverts commit 6d9dd1f5ce96ccd539e37e66f381c97d14484973, reversing
    changes made to ada78880f026b1eef3f964848a0138a2ce0e7f25.

--- SwarmLab-UPM/Runtime/Rules/CohesionRule.cs
+++ SwarmLab-UPM/Runtime/Rules/CohesionRule.cs
@@ -9,35 +9,7 @@ namespace Runtime.Rules
     {
         public override Vector3 CalculateForce(Entity entity, List<Entity> neighbors)
         {
-            if (neighbors == null || neighbors.Count == 0)
-                return Vector3.zero;
-
-            Vector3 centerOfMass = Vector3.zero;
-            float totalWeight = 0f;
-            int count = 0;
-            
-            
-            foreach (var neighbor in neighbors)
-            {
-                // Skip self if included
-                if (neighbor == entity) continue;
-
-                float weight = GetWeightFor(neighbor.Species);
-                if (weight > 0f)
-                {
-                    centerOfMass += neighbor.Position * weight;
-                    totalWeight += weight;
-                    count++;
-                }
-            }
-
-            if (count > 0 && totalWeight > 0f)
-            {
-                centerOfMass /= totalWeight;
-                Vector3 direction = centerOfMass - entity.Position;
-                return direction.normalized;
-            }
-
+            // TODO
             return Vector3.zero;
         }
     }
